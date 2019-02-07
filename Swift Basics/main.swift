b63b8c27b37f632330e90076428b0299cbd45113
Ryan Stephens <rdsfkb@gmail.com>
2019-02-06 23:15:27 -0600

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Ryan Stephens on 2/6/19.
+//  Copyright © 2019 Ryan Stephens. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
